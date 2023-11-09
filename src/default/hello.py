63ba8c097321c780dd769c4d5a112c1e0d7478f6
Piotr Gródek <128714159+piotr-grodek-dsai@users.noreply.github.com>
2023-11-09 16:45:53 +0100

    Update hello.py

--- src/default/hello.py
+++ src/default/hello.py
@@ -1,5 +1,9 @@
 """Remove this file. Required only to run coverage on empty project."""
 
+def bye_world() -> None:
+    """Says bye"""
+    print("bye bye")
+
 
 def hello_world() -> None:
     """Says hello world."""
